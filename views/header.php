a195da80c4197d5f771734e836914161d40b4503
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-10-10 00:11:13 +0300

    подключение БД

---
+++ views/header.php
@@ -0,0 +1,6 @@
+<div class="header">
+    <div class="header__nav">
+        <a class="nav__link" href="/login">login</a>
+        <a class="nav__link" href="/registration">register</a>
+    </div>
+</div>
