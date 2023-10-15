6a35a081ad40ec81bc691cbbbfca7cca754f8b0f
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-10-15 22:42:06 +0300

    добавлен сид дефолтного админа

---
+++ database/seeds/insert_default_admin.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, email, password)
+VALUES ('Admin', 'admin@mail.com', '$2y$10$6/9fErdlpnyD5szqqd4GF.Ds/Kdx1mE/kXAJ0hrmktHYZtdJbAkCy');
+
+INSERT INTO user_roles (user_id, role_id)
+VALUES ('1', '1');
