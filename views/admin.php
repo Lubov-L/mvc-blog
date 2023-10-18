47f167443028e6500343f903d3a444bb907a0265
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-10-18 22:01:26 +0300

    дизайн страницы админа

--- views/admin.php
+++ views/admin.php
@@ -4,4 +4,32 @@ require_once __DIR__ . '/head.php';
 <?php
 require_once __DIR__ . '/header.php';
 ?>
-<div class="admin">Admin page</div>
+<div class="content">
+    <div class="content__block">
+        <div class="content__panel">
+            <a href="#" class="content__panel-link">
+                Articles
+            </a>
+            <div class="line"></div>
+            <a href="#" class="content__panel-link">
+                Users
+            </a>
+            <div class="line"></div>
+            <a href="#" class="content__panel-link">
+                News
+            </a>
+        </div>
+        <div class="content__main">
+            <div class="article">
+                <h3>Validation</h3>
+                <p>
+                    Валидация на стороне клиента — это первичная проверка введённых данных, которая существенно улучшает
+                    удобство взаимодействия с интерфейсом; обнаружение некорректных данных на стороне клиента позволяет
+                    пользователю немедленно их исправить. Если же проверка происходит только на сервере, процесс заполнения
+                    может быть более трудоёмким, так как требует повторения одних и тех же действий отправки данных на
+                    сервер для получения обратного ответа с сообщением о том, что нужно исправить.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
