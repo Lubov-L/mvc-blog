7e0b5c1180c7fe2fc91d715f29f7ea0c42b1cd22
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-10-19 23:19:19 +0300

    добавлен скрипт авторизации

--- views/login.php
+++ views/login.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/head.php';
 <?php
 require_once __DIR__ . '/header.php';
 ?>
+<script async src="/js/auth.js"></script>
+
 <form action="/login" method="post" class="login__form">
     <p>Login</p>
     <label>
@@ -12,7 +14,7 @@ require_once __DIR__ . '/header.php';
     <label>
         <input type="password" name="password" placeholder="Enter your password">
     </label>
-    <button>enter</button>
+    <button type="submit" class="login_button">enter</button>
 </form>
 </body>
 </html>
