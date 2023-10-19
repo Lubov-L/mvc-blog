bf6f739f6008707f6f2e5b36314b38379530a561
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-10-19 23:17:02 +0300

    добавлена 404 для api

--- app/Controllers/ErrorController.php
+++ app/Controllers/ErrorController.php
@@ -4,8 +4,22 @@ namespace MvcBlog\App\Controllers;
 
 class ErrorController
 {
-    public static function notFound(): void
+    private static function setHeader(): void
     {
-        require_once __DIR__ . '/../../views/notFound.php';
+        header("HTTP/1.1 404 Not Found");
+    }
+
+    public static function notFound(): string
+    {
+        self::setHeader();
+
+        return require_once __DIR__ . '/../../views/notFound.php';
+    }
+
+    public static function apiNotFound(): string
+    {
+        self::setHeader();
+
+        return json_encode(['success' => false, 'error' => 'not found']);
     }
 }
