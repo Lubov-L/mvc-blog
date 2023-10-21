3a7a5340c7217e13f7907fc366fcb921946f5f39
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-10-21 21:43:28 +0300

    создана директория панели админа

--- views/admin-panel/index.php
+++ views/admin-panel/index.php
@@ -1,8 +1,8 @@
 <?php
-require_once __DIR__ . '/head.php';
+require_once __DIR__ . '/../head.php';
 ?>
 <?php
-require_once __DIR__ . '/header.php';
+require_once __DIR__ . '/../header.php';
 ?>
 <div class="content">
     <div class="content__block">
