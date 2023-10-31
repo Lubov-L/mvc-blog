c46c4c39a5e518b4df664eea1d5678bfe56e20f3
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-10-31 23:03:39 +0300

    #9 Создание дашборда для админ панели

--- views/admin-panel/index.php
+++ views/admin-panel/index.php
@@ -4,37 +4,57 @@ require_once __DIR__ . '/../head.php';
 <?php
 require_once __DIR__ . '/../header.php';
 ?>
-<script src="/js/articles.js" async></script>
-<script src="/js/content.js" async></script>
+
+<script src="/js/admin.js" async></script>
+<script src="/js/users.js" async></script>
 
 <div class="content">
     <div class="content__block">
         <div class="content__panel">
-            <a href="#" class="content__panel-link link-active" data-target-id="0">
-                Articles
+            <a href="#" class="content__panel-link" id="dashboard-link">
+                Dashboard
             </a>
             <div class="line"></div>
-            <a href="#" class="content__panel-link users_link" data-target-id="1">
+            <a href="#" class="content__panel-link" id="users-link">
                 Users
             </a>
-            <div class="line"></div>
-            <a href="#" class="content__panel-link" data-target-id="2">
-                News
-            </a>
         </div>
-        <div id="articles-container" class="content__main link-pane link-pane-show" data-id="0"></div>
-        <div id="users-container" class="content__main link-pane" data-id="1">
-            <div id="user-list"></div>
-            <div id="pagination">
-                <button id="prev-page">
-                </button>
-                <span id="current-page"></span>
-                <button id="next-page">
-                </button>
+        <div class="content__main">
+            <div class="dashboard">
+                <h3>Statistics</h3>
+                <div class="statistics">
+                    <div class="count_users">
+                        <img src="/images/users.jpg" alt="Users">
+                        <p id="count_users"></p>
+                        users
+                    </div>
+                    <div class="count_articles">
+                        <img src="/images/articles.jpg" alt="Articles">
+                        <p id="count_articles"></p>
+                        articles
+                    </div>
+                    <div class="count_comments">
+                        <img src="/images/comments.jpg" alt="Comments">
+                        <p id="count_comments"></p>
+                        comments
+                    </div>
+                    <div class="count_news">
+                        <img src="/images/news.jpg" alt="News">
+                        <p id="count_news"></p>
+                        news
+                    </div>
+                </div>
+            </div>
+            <div class="users">
+                <h3>USERS</h3>
+                <div id="user-list"></div>
+                <div id="pagination">
+                    <button id="prev-page">
+                    </button>
+                    <span id="current-page"></span>
+                    <button id="next-page"></button>
+                </div>
             </div>
         </div>
-        <div id="news-container" class="content__main link-pane" data-id="2">news</div>
     </div>
 </div>
-
-<script src="/js/users.js" async></script>
