21a16f95f4b7b30e82d0e2ed50ec5bfbb28b1150
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-11-05 22:15:40 +0300

    #12 модальное окно для создания новости

---
+++ views/news-modal.php
@@ -0,0 +1,11 @@
+<div class="wrapper">
+    <div class="back"></div>
+    <form action="" method="post" class="modal_news">
+        <a class="close-button"></a>
+        <label class="name" for="news-title">Title</label>
+        <input type="text" name="title" id="news-title">
+        <label for="context">Context</label>
+        <textarea id="context"></textarea>
+        <button type="submit" class="save-news" onclick="">create</button>
+    </form>
+</div>
