2809351335ff98973dcf6d24c08d9d0c49c8acd0
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-11-07 22:12:13 +0300

    #12 рефакторинг миграции

--- database/migrations/2023_11_05_001_create_news_table.sql
+++ database/migrations/2023_11_05_001_create_news_table.sql
@@ -3,5 +3,5 @@ CREATE TABLE news
     id               INT AUTO_INCREMENT PRIMARY KEY,
     title            VARCHAR(255) NOT NULL,
     content          TEXT         NOT NULL,
-    publication_date DATETIME     NOT NULL
+    publication_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
