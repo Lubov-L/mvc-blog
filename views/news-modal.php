f1c4d01f6f0e0cfc6fcfa12f5d83d4bf3c6b09d5
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-11-09 22:42:39 +0300

    #12 модальное окно для редактирования новости

--- views/news-modal.php
+++ views/news-modal.php
@@ -1,17 +1,38 @@
+<!--Модальное окно для создания новостей-->
 <div class="wrapper hidden">
     <div class="back"></div>
     <form action="" method="post" class="modal_news">
         <a class="close-button"></a>
+        <label for="news-id" class="news-id" hidden="hidden">
+            <input type="text" id="news-id" name="id">
+        </label>
         <label class="name" for="news-title">Title</label>
         <input type="text" name="title" id="news-title">
-        <label for="content">Context</label>
-        <textarea id="content" name="content"></textarea>
+        <label for="content">Content</label>
+        <textarea id="content" name="content" placeholder="Minimum 500 characters long"></textarea>
         <div class="messages"></div>
-        <button type="submit" class="save-news" onclick="createNews()">create</button>
+        <button type="submit" class="submit-button">create</button>
     </form>
 </div>
+<!--Модальное окно для редактирования новостей-->
+<div class="wrapper-edit hidden">
+    <div class="back-edit"></div>
+    <form action="" method="post" class="modal_edit">
+        <a class="edit-close-button"></a>
+        <label for="edit-news-id" class="news-id" hidden="hidden">
+            <input type="text" id="edit-news-id" name="id">
+        </label>
+        <label class="name" for="edit-news-title">Title</label>
+        <input type="text" name="title" id="edit-news-title">
+        <label for="edit-content">Content</label>
+        <textarea id="edit-content" name="content" placeholder="Minimum 500 characters long"></textarea>
+        <div class="messages"></div>
+        <button type="submit" class="save-button">save</button>
+    </form>
+</div>
+<!--Модальное окно для подтверждения удаления новости-->
 <div class="wrapper-delete hidden">
-    <div class="back"></div>
+    <div class="back-delete"></div>
     <div id="delete-modal">
         <div class="modal-content">
             <p>Are you sure you want to delete this news?</p>
@@ -22,5 +43,4 @@
         </div>
     </div>
 </div>
-
 
