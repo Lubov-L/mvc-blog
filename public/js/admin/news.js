718e1d20ef3140f30a89821d431ac3b383209ab0
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-11-11 15:43:27 +0300

    #13 добавлены условия для вызова функций новостей админа

--- public/js/admin/news.js
+++ public/js/admin/news.js
@@ -1,99 +1,40 @@
 /*
-Удаление новости
- */
-function newsDelete() {
-    const deleteButtons = document.querySelectorAll(".delete-news-button");
-    const wrapper = document.querySelector(".wrapper-delete");
-    const confirmDeleteButton = document.getElementById("confirm-delete");
-    const cancelDeleteButton = document.getElementById("cancel-delete");
-
-    function handleClick(e) {
-        e.preventDefault();
-
-        const parentElement = this.closest("[data-id]");
-        const dataIdValue = parentElement.getAttribute("data-id");
-
-        wrapper.classList.remove("hidden");
-
-        cancelDeleteButton.addEventListener("click", () => {
-            wrapper.classList.add("hidden");
-        });
-
-        confirmDeleteButton.addEventListener("click", () => {
-            const jsonData = {};
-            let url = '/api/v1/news';
-
-            jsonData["id"] = dataIdValue;
-
-            let requestOptions = {
-                method: "DELETE", headers: {
-                    "Content-Type": "application/json"
-                }, body: JSON.stringify(jsonData)
-            };
-
-            fetch(url, requestOptions)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error("An unexpected error occurred" + response.status);
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    if (data) {
-                        if (data.success === true) {
-                            wrapper.classList.add("hidden");
-                            location.reload();
-                        } else if (data.success === false) {
-                        }
-                    }
-                })
-                .catch(error => {
-                    console.error("An error occurred:", error);
-                });
-        });
-    }
-
-    deleteButtons.forEach(button => {
-        button.addEventListener("click", handleClick);
-    });
-}
-
-/*
 Создание новости
  */
 function showCreateModal() {
+    if (document.querySelector(".create-news")) {
+        const wrapperModal = document.querySelector(".wrapper");
+        const backModal = document.querySelector(".back");
+        const closeButton = document.querySelector(".close-button");
 
-    const wrapperModal = document.querySelector(".wrapper");
-    const backModal = document.querySelector(".back");
-    const closeButton = document.querySelector(".close-button");
-
-    function closeModal() {
-        wrapperModal.classList.toggle("hidden");
-        backModal.removeEventListener("click", closeModal);
-        closeButton.removeEventListener("click", closeModal);
-        modalNews.reset();
-        showMessage();
-    }
-
-    const modalNews = document.querySelector(".modal_news");
-    if (modalNews) {
-        modalNews.removeEventListener("submit", submitNews);
-        modalNews.addEventListener("submit", submitNews);
-    }
-
-    document.querySelector(".create-news").addEventListener("click", async function (e) {
-        e.preventDefault();
-
-        function loadDataAndDisplay() {
-            wrapperModal.classList.remove("hidden");
+        function closeModal() {
+            wrapperModal.classList.toggle("hidden");
+            backModal.removeEventListener("click", closeModal);
+            closeButton.removeEventListener("click", closeModal);
+            modalNews.reset();
             showMessage();
         }
 
-        loadDataAndDisplay();
+        const modalNews = document.querySelector(".modal_news");
+        if (modalNews) {
+            modalNews.removeEventListener("submit", submitNews);
+            modalNews.addEventListener("submit", submitNews);
+        }
 
-        backModal.addEventListener("click", closeModal);
-        closeButton.addEventListener("click", closeModal);
-    });
+        document.querySelector(".create-news").addEventListener("click", async function (e) {
+            e.preventDefault();
+
+            function loadDataAndDisplay() {
+                wrapperModal.classList.remove("hidden");
+                showMessage();
+            }
+
+            loadDataAndDisplay();
+
+            backModal.addEventListener("click", closeModal);
+            closeButton.addEventListener("click", closeModal);
+        });
+    }
 }
 
 function submitNews(e) {
@@ -143,103 +84,170 @@ function submitNews(e) {
 }
 
 /*
+Удаление новости
+ */
+function newsDelete() {
+    if (document.querySelector(".delete-news-button")) {
+        const deleteButtons = document.querySelectorAll(".delete-news-button");
+        const wrapper = document.querySelector(".wrapper-delete");
+        const confirmDeleteButton = document.getElementById("confirm-delete");
+        const cancelDeleteButton = document.getElementById("cancel-delete");
+
+        function handleClick(e) {
+            e.preventDefault();
+
+            const parentElement = this.closest("[data-id]");
+            const dataIdValue = parentElement.getAttribute("data-id");
+
+            wrapper.classList.remove("hidden");
+
+            cancelDeleteButton.addEventListener("click", () => {
+                wrapper.classList.add("hidden");
+            });
+
+            confirmDeleteButton.addEventListener("click", () => {
+                const jsonData = {};
+                let url = '/api/v1/news';
+
+                jsonData["id"] = dataIdValue;
+
+                let requestOptions = {
+                    method: "DELETE", headers: {
+                        "Content-Type": "application/json"
+                    }, body: JSON.stringify(jsonData)
+                };
+
+                fetch(url, requestOptions)
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error("An unexpected error occurred" + response.status);
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        if (data) {
+                            if (data.success === true) {
+                                wrapper.classList.add("hidden");
+                                location.reload();
+                            } else if (data.success === false) {
+                            }
+                        }
+                    })
+                    .catch(error => {
+                        console.error("An error occurred:", error);
+                    });
+            });
+        }
+
+        deleteButtons.forEach(button => {
+            button.addEventListener("click", handleClick);
+        });
+    }
+}
+
+/*
 Редактированние новости
  */
 function editNews() {
-    const wrapper = document.querySelector(".wrapper-edit");
-    const back = document.querySelector(".back-edit");
-    const closeButton = document.querySelector(".edit-close-button");
-    const editButton = document.querySelectorAll(".edit-news-button");
-    const id = document.getElementById("edit-news-id");
-    const title = document.getElementById("edit-news-title");
-    const content = document.getElementById("edit-content");
+    if (document.querySelector(".edit-close-button")) {
+        const wrapper = document.querySelector(".wrapper-edit");
+        const back = document.querySelector(".back-edit");
+        const closeButton = document.querySelector(".edit-close-button");
+        const editButton = document.querySelectorAll(".edit-news-button");
+        const id = document.getElementById("edit-news-id");
+        const title = document.getElementById("edit-news-title");
+        const content = document.getElementById("edit-content");
 
-    async function handleClick(e) {
-        e.preventDefault();
+        async function handleClick(e) {
+            e.preventDefault();
 
-        let parentElement = this.closest("[data-id]");
-        let dataIdValue = parentElement.getAttribute("data-id");
+            let parentElement = this.closest("[data-id]");
+            let dataIdValue = parentElement.getAttribute("data-id");
 
-        async function loadData(dataIdValue) {
-            try {
-                const response = await fetch(`/api/v1/news?id=${dataIdValue}`);
-                return await response.json();
-            } catch (error) {
-                console.error("Data error:", error);
+            async function loadData(dataIdValue) {
+                try {
+                    const response = await fetch(`/api/v1/news?id=${dataIdValue}`);
+                    return await response.json();
+                } catch (error) {
+                    console.error("Data error:", error);
+                }
             }
+
+            async function loadDataAndDisplay() {
+                wrapper.classList.remove("hidden");
+
+                const data = await loadData(dataIdValue);
+                const item = data.news;
+
+                id.value = item.id
+                title.value = item.title;
+                content.value = item.content;
+            }
+
+            await loadDataAndDisplay();
         }
 
-        async function loadDataAndDisplay() {
-            wrapper.classList.remove("hidden");
+        editButton.forEach(button => {
+            button.addEventListener("click", handleClick);
+        });
 
-            const data = await loadData(dataIdValue);
-            const item = data.news;
-
-            id.value = item.id
-            title.value = item.title;
-            content.value = item.content;
+        function closeModal() {
+            wrapper.classList.toggle("hidden");
         }
 
-        await loadDataAndDisplay();
+        back.addEventListener("click", closeModal);
+        closeButton.addEventListener("click", closeModal);
     }
-
-    editButton.forEach(button => {
-        button.addEventListener("click", handleClick);
-    });
-
-    function closeModal() {
-        wrapper.classList.toggle("hidden");
-    }
-
-    back.addEventListener("click", closeModal);
-    closeButton.addEventListener("click", closeModal);
 }
 
 /*
 Сохрание редактированной новости
  */
 function saveEditedNews() {
-    document.querySelector(".modal_edit").addEventListener("submit", async function (e) {
-        e.preventDefault();
+    if (document.querySelector(".modal_edit")) {
+        document.querySelector(".modal_edit").addEventListener("submit", async function (e) {
+            e.preventDefault();
 
-        const wrapperModal = document.querySelector(".wrapper-edit");
+            const wrapperModal = document.querySelector(".wrapper-edit");
 
-        let form = document.querySelector(".modal_edit");
-        let formData = new FormData(form);
-        let jsonData = {};
+            let form = document.querySelector(".modal_edit");
+            let formData = new FormData(form);
+            let jsonData = {};
 
-        formData.forEach(function (value, key) {
-            jsonData[key] = value;
-        });
-
-        let url = "/api/v1/news";
-
-        let requestOptions = {
-            method: "PUT", headers: {
-                "Content-Type": "application/json"
-            }, body: JSON.stringify(jsonData)
-        };
-
-        fetch(url, requestOptions)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("An unexpected error occurred" + response.status);
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (data) {
-                    if (data.success === true) {
-                        showMessage("success", data.message);
-                        alert("Successfully saved!")
-                        wrapperModal.classList.toggle("hidden");
-                    } else {
-                        showMessage("error", data.message);
-                    }
-                }
-            })
-            .catch(error => {
-                showMessage("error", "Failed to save:" + error.message);
+            formData.forEach(function (value, key) {
+                jsonData[key] = value;
             });
-    });
+
+            let url = "/api/v1/news";
+
+            let requestOptions = {
+                method: "PUT", headers: {
+                    "Content-Type": "application/json"
+                }, body: JSON.stringify(jsonData)
+            };
+
+            fetch(url, requestOptions)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error("An unexpected error occurred" + response.status);
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    if (data) {
+                        if (data.success === true) {
+                            showMessage("success", data.message);
+                            alert("Successfully saved!")
+                            wrapperModal.classList.toggle("hidden");
+                            location.reload();
+                        } else {
+                            showMessage("error", data.message);
+                        }
+                    }
+                })
+                .catch(error => {
+                    showMessage("error", "Failed to save:" + error.message);
+                });
+        });
+    }
 }
