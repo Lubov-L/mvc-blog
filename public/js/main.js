c23828696f1efc69b18d405dcf2cee9098e1679a
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-11-19 20:16:38 +0300

    #28 Перенос дублирующейся функции в отдельный файл для переиспользования

--- public/js/main.js
+++ public/js/main.js
@@ -22,3 +22,76 @@ function getCurrentPage() {
 function saveCurrentPage(page) {
     localStorage.setItem('currentPage', page.toString());
 }
+
+// Пагинация
+function generatePagination(countPage, currentPage, paginationContainer, callback) {
+    paginationContainer.innerHTML = '';
+
+    if (countPage > 3) {
+        if (currentPage > 2) {
+            const firstPage = document.createElement('button');
+            const prevEllipsis = document.createElement('a');
+
+            firstPage.classList.add('pagination-item');
+            prevEllipsis.classList.add('pagination-dot', 'disabled');
+            prevEllipsis.textContent = '...';
+            firstPage.textContent = '1';
+            paginationContainer.appendChild(firstPage);
+            paginationContainer.appendChild(prevEllipsis);
+        }
+
+        if (currentPage <= countPage - 2) {
+            const nextEllipsis = document.createElement('a');
+            const lastPage = document.createElement('button');
+
+            for (let i = Math.max(1, currentPage - 1); i <= Math.min(countPage, currentPage + 1); i++) {
+                const paginationItem = document.createElement('button');
+                paginationItem.classList.add('pagination-item');
+                if (i === currentPage) {
+                    paginationItem.classList.add('active-pagination');
+                }
+                paginationItem.textContent = i.toString();
+                paginationContainer.appendChild(paginationItem);
+            }
+            nextEllipsis.classList.add('pagination-dot', 'disabled');
+            paginationContainer.appendChild(nextEllipsis);
+            nextEllipsis.textContent = '...';
+
+            lastPage.classList.add('pagination-item');
+            lastPage.textContent = countPage;
+            paginationContainer.appendChild(lastPage);
+        } else {
+            for (let i = Math.max(1, countPage - 2); i <= countPage; i++) {
+                const paginationItem = document.createElement('button');
+                paginationItem.classList.add('pagination-item');
+                if (i === currentPage) {
+                    paginationItem.classList.add('active-pagination');
+                }
+                paginationItem.textContent = i.toString();
+                paginationContainer.appendChild(paginationItem);
+            }
+        }
+    } else {
+        // Если количество страниц меньше или равно 3, создавать троеточие не нужно
+        for (let i = 1; i <= countPage; i++) {
+            const paginationItem = document.createElement('button');
+            paginationItem.classList.add('pagination-item');
+            if (i === currentPage) {
+                paginationItem.classList.add('active-pagination');
+            }
+            paginationItem.textContent = i.toString();
+            paginationContainer.appendChild(paginationItem);
+        }
+    }
+
+    function handlePaginationClick(event) {
+        if (event.target.classList.contains('pagination-item') && !event.target.classList.contains('disabled')) {
+            currentPage = parseInt(event.target.textContent);
+            saveCurrentPage(currentPage);
+            window.scrollTo(0, 0);
+            callback();
+        }
+    }
+
+    paginationContainer.addEventListener('click', handlePaginationClick);
+}
